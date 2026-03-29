81359fc8a239b3d34ffcf5f2f392571f93dacf31
Ayeshaali124 <ayeshansari124@gmail.com>
2026-03-29 15:47:35 +0530

    fix: doctor

--- src/app/api/(medicare)/doctors/[id]/route.js
+++ src/app/api/(medicare)/doctors/[id]/route.js
@@ -3,12 +3,11 @@ import { NextResponse } from "next/server";
 
 export async function GET(req, { params }) {
 
-  const { id } = params;
+  const { id } = await params; 
 
   const doctor = await prisma.doctor.findUnique({
     where: { id }
   });
 
   return NextResponse.json({ doctor });
-
 }
